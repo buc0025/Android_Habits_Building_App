30bb45eb1cc6a84c53e8c1a7957591c1ba41bafe
buc0025 <stanley.yang001@gmail.com>
2020-12-16 17:37:09 -0500

    Weekview with added dates
    Implemented
    -Display current dates on top of cardview images of days of the week
    -Calendar for display of current dates uses logic for adding and subtracting current date
    
    TODO
    -Create second table to hold dates of completed habits for the day

--- app/src/main/java/com/example/habitscalendar/activities/ListViewAdapter.java
+++ app/src/main/java/com/example/habitscalendar/activities/ListViewAdapter.java
@@ -16,6 +16,10 @@ import com.example.habitscalendar.R;
 import com.example.habitscalendar.models.Habit;
 import com.example.habitscalendar.models.WeekDayAdapter;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class ListViewAdapter extends ArrayAdapter<Habit> {
@@ -40,11 +44,59 @@ public class ListViewAdapter extends ArrayAdapter<Habit> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         String habitName = habitList.get(position).getHabit();
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_YEAR, -3);
+        Date sunday = calendar.getTime();
+
+        calendar.add(Calendar.DAY_OF_YEAR, 1);
+        Date monday = calendar.getTime();
+
+        calendar.add(Calendar.DAY_OF_YEAR, 1);
+        Date tuesday = calendar.getTime();
+
+        calendar.add(Calendar.DAY_OF_YEAR, 1);
+        Date wednesday = calendar.getTime();
+
+        calendar.add(Calendar.DAY_OF_YEAR, 1);
+        Date thursday = calendar.getTime();
+
+        calendar.add(Calendar.DAY_OF_YEAR, 1);
+        Date friday = calendar.getTime();
+
+        calendar.add(Calendar.DAY_OF_YEAR, 1);
+        Date saturday = calendar.getTime();
+
+        DateFormat dateFormat = new SimpleDateFormat("MM/dd");
+
+        String sundayAsString = dateFormat.format(sunday);
+        String mondayAsString = dateFormat.format(monday);
+        String tuesdayAsString = dateFormat.format(tuesday);
+        String wednesdayAsString = dateFormat.format(wednesday);
+        String thursdayAsString = dateFormat.format(thursday);
+        String fridayAsString = dateFormat.format(friday);
+        String saturdayAsString = dateFormat.format(saturday);
+
         // inflating recyclerView
         View view = layoutInflater.inflate(R.layout.habit_row, null, false);
         TextView habitTextView = (TextView) view.findViewById(R.id.habitName);
         habitTextView.setText(habitName);
-        
+
+        TextView sun = view.findViewById(R.id.sunday);
+        sun.setText(sundayAsString);
+        TextView mon = view.findViewById(R.id.monday);
+        mon.setText(mondayAsString);
+        TextView tues = view.findViewById(R.id.tuesday);
+        tues.setText(tuesdayAsString);
+        TextView wed = view.findViewById(R.id.wednesday);
+        wed.setText(wednesdayAsString);
+        TextView thur = view.findViewById(R.id.thursday);
+        thur.setText(thursdayAsString);
+        TextView fri = view.findViewById(R.id.friday);
+        fri.setText(fridayAsString);
+        TextView sat = view.findViewById(R.id.saturday);
+        sat.setText(saturdayAsString);
+
         RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
         recyclerView.setAdapter(weekDayAdapter);
 
